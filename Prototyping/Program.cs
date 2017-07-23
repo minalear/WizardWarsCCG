d5eefd2baed034a5fe502c449989289d39e55505
Trevor Fisher <minalear@gmail.com>
2017-07-23 00:48:17 -0500

    Created a project to prototype features and test ideas.  Testing JSON loading of card meta info.

---
+++ Prototyping/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using WizardWars;
+
+namespace Prototyping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("Content/cards.json");
+            List<Card> cardList = JsonConvert.DeserializeObject<List<Card>>(json);
+
+            foreach (Card card in cardList)
+            {
+                PrintCardInfo(card);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void PrintCardInfo(Card card)
+        {
+            Console.WriteLine("{0} - {1}", card.Name, card.Cost);
+
+            foreach (string type in card.Types)
+            {
+                Console.Write("{0} ", type);
+            }
+
+            if (card.SubTypes.Length > 0)
+            {
+                Console.Write(" - ");
+                foreach (string subType in card.SubTypes)
+                {
+                    Console.Write("{0} ", subType);
+                }
+            }
+
+            Console.Write("\n");
+
+            Console.WriteLine(" --- \n{0}\n --- \n{1}", card.RulesText, card.FlavorText);
+
+            Console.Write("\n\n");
+        }
+    }
+}
---
+++ WizardWars/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardWars
+{
+    public class Card
+    {
+        public string Name;
+        public int Cost;
+        public string RulesText;
+        public string FlavorText;
+
+        public string[] Types;
+        public string[] SubTypes;
+
+        public Card()
+        {
+            Types = new string[0];
+            SubTypes = new string[0];
+        }
+    }
+}
